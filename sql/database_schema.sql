d8b66d931e1b5c40de285b862e9c023238d36097
Conrado Basso <79751069+conradocabelo@users.noreply.github.com>
2022-07-22 10:02:39 -0300

    Adicionado schema da base de dados

---
+++ sql/database_schema.sql
@@ -0,0 +1,34 @@
+create table if not exists grupo (
+   id int GENERATED ALWAYS AS IDENTITY,
+   descricao varchar(255),
+   constraint pk_grupo primary key (id)
+)  
+
+create table if not exists alimento (
+   id int GENERATED ALWAYS AS IDENTITY,
+   id_grupo int not null,
+   codigo int not null,
+   descricao varchar(255) not null,
+   constraint pk_alimento primary key (id),
+   constraint fk_alimento_grupo foreign key (id_grupo) references grupo(id)
+)
+
+create table if not exists tipo_preparo (
+   id int GENERATED ALWAYS AS IDENTITY,
+   codigo int not null,
+   descricao varchar(255) not null,
+   constraint pk_tipo_preparo primary key (id)
+)
+
+create table if not exists valor_energetico (
+   id_alimento int not null,
+   id_tipo_preparo int not null,
+   energia decimal,
+   proteina decimal,
+   lipidios_totais decimal,
+   carboidratos decimal,
+   fibra_alimentar_total decimal,
+   constraint pk_valor_energetico primary key (id_alimento, id_tipo_preparo),
+   constraint fk_valor_energetico_alimento foreign key (id_alimento) references alimento(id),
+   constraint fk_valor_energetico_tipo_preparo foreign key (id_tipo_preparo) references tipo_preparo(id)
+)
